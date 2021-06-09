bbf09cd95bf5c5b22ded40fc3968ea1683c1215b
NikolaiSurodin <kolya_12.93@mail.ru>
2021-06-09 18:57:25 +0700

    refactor

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <form class="form-signin" @submit.prevent="submitLogin()">
+    <form class="form" @submit.prevent="submitLogin">
       <img class="mb-4" src="../assets/2017679.png" alt width="240" height="105">
       <h1 class="h3 mb-3 font-weight-normal">Вход</h1>
       <p>Введите данные для входа и нажмите Войти:</p>
-
+      <label for="inputEmail" class="row ml-0 p-0"><b-icon icon="at" aria-hidden="true"></b-icon></label>
       <input type="email"
              id="inputEmail"
              class="form-control"
-             placeholder="Email"
+             placeholder="email"
              required autofocus
              v-model.trim="user.email"
              :class="$v.user.email.$error ? 'is-invalid' : ''"
       >
-      <p v-if="$v.user.password.$dirty && !$v.user.password.minLength" class="invalid-feedback">Обязательное поле!</p>
       <p v-if="$v.user.email.$dirty && !$v.user.email.email" class="invalid-feedback">Введите корректный E-mail</p>
-
+      <p v-if="$v.user.email.$dirty && !$v.user.email.required" class="invalid-feedback">Обязательное поле!</p>
+      <label for="inputPassword" class="row ml-0 mt-1"><b-icon icon="lock-fill" aria-hidden="true"></b-icon></label>
       <input type="password"
              id="inputPassword"
              class="form-control"
@@ -33,7 +33,7 @@
         <b-button class="mt-3" variant="outline-success" type="submit">Войти</b-button>
 
         <router-link to="/">Регистрация</router-link>
-        <p class="mt-5 mb-3 text-muted">© GDS - 2021</p>
+        <p class="mt-5 mb-3 text-muted">© GDS - {{ currentYear }}</p>
       </div>
     </form>
   </div>
@@ -78,6 +78,11 @@ export default {
         this.$emit('submitLogin', this.user)
       }
     }
+  },
+  computed:{
+    currentYear() {
+      return new Date().getFullYear()
+    }
   }
 }
 
@@ -85,64 +90,17 @@ export default {
 </script>
 
 <style scoped>
-.form-signin {
+.form {
   width: 100%;
   max-width: 330px;
   padding: 15px;
   margin: auto;
 }
-
-form {
-  display: block;
-  margin-top: 0;
-}
-
-.text-center {
-  text-align: center !important;
-}
-
-h1 {
-  display: block;
-  font-size: 2em;
-  margin-block-start: 0.67em;
-  margin-block-end: 0.67em;
-  margin-inline-start: 0;
-  margin-inline-end: 0;
-  font-weight: bold;
-}
-
-.sr-only {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  border: 0;
-}
-
-label {
-  display: inline-block;
-  margin-bottom: .5rem;
-}
-
-label {
-  cursor: default;
-}
-
-.text-center {
-  text-align: center !important;
-}
-
 .btn {
   margin-bottom: 50px;
   width: 100%;
 }
 
-.element {
-  display: flex;
-  flex-direction: column;
-}
+
 
 </style>
